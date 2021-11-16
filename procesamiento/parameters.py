d193f4d633b5b7701d4732536a970c74eb650379
AGUSTIN CARACCI <juan.caracci@uc.cl>
2021-11-16 18:25:50 -0300

    Update parameters.py

---
+++ procesamiento/parameters.py
@@ -0,0 +1,13 @@
+### Parametros del modelo ###
+
+# Tiempo minimo por perido de asignacion para un abogado
+T_MIN = 0
+
+# Factor de penalizacion por abogado extra asignado a un servicio
+BETA = 1
+
+# Factor de penalizacion por servicio no asignado
+GAMMA = 1
+
+# Factor de penalizacion temporal
+LAMBDA = 1
