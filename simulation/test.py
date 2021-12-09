ad0077394d539213a7d7131a1a82d9bf1b3bce8b
JORGE OVALLE <jorge.ovalle@uc.cl>
2021-12-09 20:31:39 -0300

    beta0

--- simulation/test.py
+++ simulation/test.py
@@ -10,8 +10,8 @@ with open(f'results/assignments.pickle', 'rb') as file:
     a = pickle.load(file)
 
 print('SAA')
-print(a[3][0.8]['saa'])
+print(a[1.25][0.8]['saa'])
 print('\n')
 
 print('GREEDY')
-print(a[3][0.8]['greedy'])
+print(a[1.25][0.8]['greedy'])
